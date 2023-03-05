a357a78d5327349e1106122bb07c131d90b6d067
Daniel Rojas <kazedynamic@gmail.com>
2023-03-04 23:15:45 -0600

    Added Auth FE and BE

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,12 @@
 export const serializeNonPOJOs = (obj) => {
 	return structuredClone(obj); //deep clone of an object that does a JSONparse and JSONstringify
 };
+
+export const generateUsername = (name) => {
+	const id = randomBytes(2).toString('hex');
+	return `${name.slice(0, 5)}${id}`;
+};
+
+export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
+	return `http://localhost:8090/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
+};
