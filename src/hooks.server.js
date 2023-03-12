03ec87732bdfc8ba51b8e27c1ce12049709bc721
Daniel Rojas <kazedynamic@gmail.com>
2023-03-12 15:18:10 -0600

    test1

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -5,20 +5,15 @@ export const handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase('http://198.74.53.179:80');
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
-	try {
-		if (event.locals.pb.authStore.isValid) {
-			//if the user is valid
-			await event.locals.pb.collection('users').authRefresh();
-			event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model); //convert it
-		}
-	} catch (_) {
-		event.locals.pb.authStore.clear();
+	if (event.locals.pb.authStore.isValid) {
+		event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
+	} else {
 		event.locals.user = undefined;
 	}
 
 	const response = await resolve(event);
 
-	response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false })); //takes all the info from the current authStore and sets it in the headers before responding to the client
+	response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }));
 
 	return response;
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,12 @@
 <script>
 	import '../app.postcss';
-	import { Toaster } from 'svelte-french-toast';
-	import { getImageURL } from '$lib/utils';
 	export let data;
 </script>
 
-<Toaster />
 <div class="min-h-full">
 	<nav class="navbar bg-base-100 border-b">
 		<div class="flex-1">
-			<a href="/" class="btn btn-ghost normal-case text-xl">Showcase</a>
+			<a href="/" class="btn btn-ghost normal-case text-xl">Regulatory Notebook</a>
 		</div>
 		<div class="flex-none">
 			{#if !data.user}
@@ -26,12 +23,7 @@
 					<!-- svelte-ignore a11y-label-has-associated-control -->
 					<label tabindex="0" class="btn btn-ghost btn-circle avatar">
 						<div class="w-10 rounded-full">
-							<img
-								src={data.user?.avatar
-									? getImageURL(data.user?.collectionId, data.user?.id, data.user?.avatar)
-									: `https://ui-avatars.com/api/?name=${data.user?.name}`}
-								alt="User avatar"
-							/>
+							<img src="https://placeimg.com/80/80/people" alt="User avatar" />
 						</div>
 					</label>
 					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
@@ -53,7 +45,7 @@
 			{/if}
 		</div>
 	</nav>
-	<div class="py-4">
+	<div class="py-10">
 		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
 			<slot />
 		</div>
