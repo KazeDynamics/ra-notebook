699539e93f46def096eddc535b49b380cc0e106d
Daniel Rojas <kazedynamic@gmail.com>
2023-03-20 22:57:15 -0600

    fdgdg

--- src/lib/components/ItemCard.svelte
+++ src/lib/components/ItemCard.svelte
@@ -4,11 +4,11 @@
 
 <div class="card w-72 bg-base-100 shadow-xl">
 	<div class="card-body">
-		<h2 class="card-title">{item.name}</h2>
+		<h2 class="card-title font-bold">{item.name}</h2>
 		{#if item.process === 'productSubmission'}
-			<p>Product Submission</p>
+			<p class="text-secondary font-medium">Product Submission</p>
 		{:else if item.process === 'productRenewal'}
-			<p>Product Renewal</p>
+			<p class="text-secondary font-medium">Product Renewal</p>
 		{/if}
 		<div class="card-actions justify-center pt-5">
 			<a href="/items/{item.id}" class="btn btn-primary">View Item</a>
--- src/routes/items/[itemsId]/+page.svelte
+++ src/routes/items/[itemsId]/+page.svelte
@@ -1,11 +1,37 @@
 <script>
 	export let data;
+	import Icon from '@iconify/svelte';
 </script>
 
 <div class="flex flex-col w-full mt-4">
-	<h1 class="text-2xl font-bold mt-4">
-		{data.item.name}
-	</h1>
-	<p class="text-2xl font-light mt-2">{data.item.country}</p>
-	<p>{data.item.process}</p>
+	<h2>
+		Product Name: <h1 class="text-3xl font-bold">{data.item.name}</h1>
+	</h2>
+	<h2 class="mt-6">
+		Country:
+		{#if data.item.country === 'guatemala'}
+			<div><Icon icon="twemoji:flag-guatemala" class="text-5xl" /></div>
+		{:else if data.item.country === 'honduras'}
+			<div><Icon icon="twemoji:flag-honduras" class="text-5xl" /></div>
+		{:else if data.item.country === 'elSalvador'}
+			<div><Icon icon="twemoji:flag-el-salvador" class="text-5xl" /></div>
+		{:else if data.item.country === 'nicaragua'}
+			<div><Icon icon="twemoji:flag-nicaragua" class="text-5xl" /></div>
+		{:else if data.item.country === 'costaRica'}
+			<div><Icon icon="twemoji:flag-costa-rica" class="text-5xl" /></div>
+		{:else if data.item.country === 'panama'}
+			<div><Icon icon="twemoji:flag-panama" class="text-5xl" /></div>
+		{/if}
+	</h2>
+	<h2 class="mt-6">
+		Process Type:
+		{#if data.item.process === 'productSubmission'}
+			<h1 class="text-3xl font-bold">Product Submission</h1>
+		{:else if data.item.process === 'productRenewal'}
+			<h1 class="text-3xl font-bold">Product Renewal</h1>
+		{/if}
+	</h2>
+	<h2 class="mt-6">
+		Business Unit: <h1 class="text-3xl font-bold">{data.item.businessUnit}</h1>
+	</h2>
 </div>
