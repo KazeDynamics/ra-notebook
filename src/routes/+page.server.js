af9474a2d1b2b52a69c2b5d73872da11796d4eea
Daniel Rojas <kazedynamic@gmail.com>
2023-05-21 21:29:12 -0600

    Production v0

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = ({ locals }) => {
-	if (!locals.pb.authStore.isValid) {
-		throw redirect(303, '/login');
-	} else {
+	if (locals.pb.authStore.isValid) {
 		throw redirect(303, '/home');
 	}
 };
