edf56c439e9694789578ff8120ced0eb86351478
Daniel Rojas <kazedynamic@gmail.com>
2023-05-24 22:19:05 -0600

    Production v0

--- src/lib/components/MyItem.svelte
+++ src/lib/components/MyItem.svelte
@@ -1031,3 +1031,17 @@
 		</div>
 	</label>
 </div>
+
+<style lang="postcss">
+	input[type='checkbox'] {
+		position: absolute;
+		width: 1px;
+		height: 1px;
+		padding: 0;
+		margin: -1px;
+		overflow: hidden;
+		clip: rect(0, 0, 0, 0);
+		white-space: nowrap;
+		border: 0;
+	}
+</style>
