5f628eced791df6a469ff6ade12ea5d1ca4918cf
Daniel Rojas <kazedynamic@gmail.com>
2023-06-02 14:53:07 -0600

    Production v1

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<div class="w-full mt-4 flex flex-col items-center">
+	<p class="text-center text-3xl">😵</p>
+	<p class="text-center text-3xl m-3">Did you lose your way? Go back to continue your journey.</p>
+	<a href="/" class="btn btn-primary max-w-md mt-4">Homepage</a>
+</div>
